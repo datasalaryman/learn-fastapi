48d84d50cbd1607f4830d8ed36991faa985812d9
Data Salaryman <datasalaryman@protonmail.com>
2022-11-22 16:16:48 +0800

    feat(learn_fastapi): create api skeleton

--- learn_fastapi/main.py
+++ learn_fastapi/main.py
@@ -5,4 +5,20 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return "Hello, I'm CRUD API"
+
+@app.post("/create")
+async def create():
+    return "Creating table in DB"
+
+@app.get("/read")
+async def read():
+    return "Reading table in DB"
+
+@app.post("/update")
+async def update():
+    return "Updating table in DB"
+
+@app.get("/delete")
+async def delete():
+    return "Deleting table in DB"
